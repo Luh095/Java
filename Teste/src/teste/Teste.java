597958a2b2c4919e8487752e77cb2803e5ec593f
Luana Roza <91756435+Luh095@users.noreply.github.com>
2024-03-26 16:34:11 -0300

    Add files via upload

---
+++ Teste/src/teste/Teste.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teste;
+
+/**
+ *
+ * @author luroz
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c = 'c';
+        int i = 10;
+        double d = 10;
+        long l = 1;
+        String s = "Hello";
+        
+    }
+    
+}
